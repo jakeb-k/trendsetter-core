99d2c38224501d8853a7afa556f845292e2fc0c1
jakeb-k <jknow30@outlook.com>
2025-05-28 18:51:12 +1000

    [FEAT] set up login api request

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function () {
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/auth/login', [AuthenticatedSessionController::class, 'storeApi'])->name('api.login'); 
+
+});
