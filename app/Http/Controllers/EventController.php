31294af9f4847d8440437baa7e5b4e750d8fd14d
jakeb-k <jknow30@outlook.com>
2025-07-26 17:49:40 +1000

    FEAT - initial setup of the events api routes and functions

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    public function getEventFeedback(Event $event)
+    {
+        //@todo Implement logic that gets the current streak count
+        return [
+            'feedback' => $event->feedback(),
+        ];
+    }
+
+    public function storeEventFeedback(Request $request, Event $event)
+    {
+        //
+    }
+
+    public function updateEventFeedback(Request $request, Event $event)
+    {
+        //
+    }
+
+    public function deleteEventFeedback(Event $event)
+    {
+        //
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,19 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\EventController;
 use App\Http\Controllers\GoalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
 Route::prefix('v1')->group(function () {
-    Route::middleware('auth:sanctum')->group(function() {
-        Route::post('/ai-plan/chat', [GoalController::class, 'generatePlan'])->name('api.ai.plan.chat'); 
-    }); 
-    Route::post('/auth/login', [AuthenticatedSessionController::class, 'storeApi'])->name('api.login'); 
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/ai-plan/chat', [GoalController::class, 'generatePlan'])->name('api.ai.plan.chat');
+        Route::get('/events/{event}/feedback', [EventController::class, 'getEventFeedback'])->name('api.event.feedback');
+        Route::post('/events/feedback', [EventController::class, 'storeEventFeedback'])->name('api.event.feedback.store');
+        Route::put('/events/{event}/feedback', [EventController::class, 'updateEventFeedback'])->name('api.event.feedback.update');
+        Route::delete('/events/{event}/feedback', [EventController::class, 'deleteEventFeedback'])->name('api.event.feedback.delete');
+    });
+    Route::post('/auth/login', [AuthenticatedSessionController::class, 'storeApi'])->name('api.login');
 });
 //I want to get better at programming and become a senior level developer in 2 years
